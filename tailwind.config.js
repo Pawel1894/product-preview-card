7d648d949b73562fe270e03b7a02a0a1ff0aebfa
Pawel1894 <pawepohl@gmail.com>
2023-01-08 13:06:07 +0100

    mobile styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     colors: {
-      "dark-cyan": "hsl(158, 36%, 37%)",
+      "dark-cyan": {
+        100: "hsl(158, 36%, 37%)",
+        200: "hsl(159, 35%, 29%)",
+      },
       cream: "hsl(30, 38%, 92%)",
       "very-dark-blue": "hsl(212, 21%, 14%)",
       "dark-grayish-blue": "hsl(228, 12%, 48%)",
@@ -13,7 +16,12 @@ module.exports = {
       serif: ["Fraunces", "serif"],
       sans: ["Montserrat", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        mobile: "url('./images/image-product-mobile.jpg')",
+        desktop: "url('./images/image-product-desktop.jpg')",
+      },
+    },
   },
   plugins: [],
 };
